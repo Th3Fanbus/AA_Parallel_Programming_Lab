507909945dceda6e045a2fdd8fd4b13cacf3c113
Angel Pons <th3fanbus@gmail.com>
2025-12-09 16:45:45 +0100

    3_Example_saxpy: Force Numba JIT compilation in advance
    Force JIT compilation of the `run_saxpy` function in advance.
    Otherwise, the execution time also includes JIT compilation.
    
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>

--- 3_Example_saxpy/c_sapxpy_numba.py
+++ 3_Example_saxpy/c_sapxpy_numba.py
@@ -9,15 +9,19 @@ def run_saxpy(x,y,a):
 if __name__ == "__main__":
 	size = 10000000
 	a = 2.1
-	
+
 	print(".")
 	x = np.random.random(size)
 	y = np.random.random(size)
+
+	# Force JIT compilation of the `run_saxpy` function in advance.
+	# Otherwise, the execution time also includes JIT compilation.
+	run_saxpy(np.random.random(1), np.random.random(1), 1.0)
 
 	print("..")
 	start = time.time()
 	out = run_saxpy(x,y,a)
 	end = (time.time()-start)
-	
+
 	print(out[:5])
 	print("Total time: %0.10f seconds"%end)
